4bb6ed5f747d7d72f8026ac5f5e9d8bdcc2995fe
Peter <naughtnowwhen@gmail.com>
2018-10-12 17:24:10 -0700

    got it working, grabs 3 rand, splices them from arr

--- app.js
+++ app.js
@@ -30,30 +30,25 @@ var getSecondImg = document.getElementById('secondPic');
 var getThirdImg = document.getElementById('thirdPic');
 var imgGetters = [getFirstImg,getSecondImg,getThirdImg];
 
-//ah oh, i misunderstood, this slice isn't what i was wanting, i though slicing at index 0 would grab every index, but no. refactor. 
 
-let GoingToBeSplicedGoatArr = goatArr.map(ele => ele);
+let GoingToBeSplicedGoatObj = goatArr.map(ele => ele);
+// ahha here's a problem, GoingToBeSplicedGoatObj isn't an array, need to turn it into an array before splicing it. 
 
-console.log(GoingToBeSplicedGoatArr);
-GoingToBeSplicedGoatArr.pop();
+//Yes, that was the right idea!
 
-let ThreeRandosGrabbed = [];
+let RandosGrabbed = [];
 let HowManyDoIWantToDisplay = 3;
 
 var randoGoatImgGrabberFun = function(){    
-
-// ah oh, this hard coded version isn't going to work, the for loop is trying to splice out the same SingleGrab, I need a unique splice each time. So, turn it into a function, huh?
-
-// let singleGrab = GoingToBeSplicedGoatArr[Math.floor(Math.random() * goatArr.length)];
-// console.log(singleGrab, "single Grab");
-// GoingToBeSplicedGoatArr.splice(singleGrab, 1);
-// ThreeRandosGrabbed.push(singleGrab);
-// console.log(GoingToBeSplicedGoatArr);
+let singleGrab = GoingToBeSplicedGoatObj[Math.floor(Math.random() * GoingToBeSplicedGoatObj.length)];
+RandosGrabbed.push(singleGrab);
+let singleGrabIndexVal = GoingToBeSplicedGoatObj.indexOf(singleGrab);
+GoingToBeSplicedGoatObj.splice(singleGrabIndexVal, 1);
 }
 
-// for (var i = 0; i < HowManyDoIWantToDisplay; i ++){
-// randoGoatImgGrabberFun();
-// }
+for (var i = 0; i < HowManyDoIWantToDisplay; i ++){
+randoGoatImgGrabberFun();
+}
 
 // var displayerFun = function (){
 // var displayerArrRandomSelection = [];
