eb6ad3e939ff4328a5a415a7ba6cbc4281ccfac9
Peter <naughtnowwhen@gmail.com>
2018-10-17 14:58:49 -0700

    committing the branch, about to put on new branch

--- folder/app.js
+++ folder/app.js
@@ -1,6 +1,15 @@
 'use strict';
 
+// Zahra mentioned that making it as a constructor to make it better.
+
 var goatArr = [];
+var totalCounts = 0;
+
+//for when it's done...
+var labelsOfNames  = [];
+var dataLikes = [];
+var dataSeen = [];
+
 
 //for testing
 let firstCheckArr = [];
@@ -20,10 +29,12 @@ var domImgGetters = [getFirstImg,getSecondImg,getThirdImg];
 var GoatConstructor = function(src, name, int, likes,timesShown){
   this.src = src;
   this.name = name;
+  // int for ease of tracking
   this.int = int;
   this.likes = 0;
   this.timesShown = 0;
-  // int for ease of tracking
+  labelsOfNames. push(this.name);
+
   goatArr.push(this);
 };
 new GoatConstructor('goatImagesExample/cruisin-goat.jpg', 'goatCruisin',0);
@@ -32,28 +43,34 @@ new GoatConstructor('goatImagesExample/goat-away.jpg', 'goatAway',2);
 new GoatConstructor('goatImagesExample/goat-out-of-hand.jpg', 'goatHand',3);
 new GoatConstructor('goatImagesExample/kissing-goat.jpg', 'GoatKiss',4);
 new GoatConstructor('goatImagesExample/sassy-goat.jpg', 'goatSassy', 5);
-new GoatConstructor('goatImagesExample/smiling-goat.jpg', 'goatSmile', 6);
-new GoatConstructor('goatImagesExample/sweater-goat.jpg', 'goatSweater',7);
+// new GoatConstructor('goatImagesExample/smiling-goat.jpg', 'goatSmile', 6);
+// new GoatConstructor('goatImagesExample/sweater-goat.jpg', 'goatSweater',7);
+// new GoatConstructor('#', 'unique',8);
+
+
+
+
 
 let randosGrabbed = [];
 let historyOfRandos = [];
 let HowManyDoIWantToDisplay = 3;
 let GoingToBeSplicedGoatObj = goatArr.map(ele => ele);
 let arg;
-let isItFromHandler;
+// let fromHandler = false;
 
-// let argDefaultsToFalse; 
+// let argDefaultsToFalse;
 //therethere
-var randoGoatImgGrabberFun = function(tester, fromHandler){
-//   console.log(fromHandler);
-  if (fromHandler === true){
-    console.log('its from the handler');
-    console.log(GoingToBeSplicedGoatObj);
+let initialVals;
+let handledVals;
+
+var therethere = 'theretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretheretherethere';
+
+var randoGoatImgGrabberFun = function(tester, fromHandler=false){
+
+  if(GoingToBeSplicedGoatObj.length === 0){
+    GoingToBeSplicedGoatObj = goatArr.map(ele => ele);
   }
-  else if (!isItFromHandler){
-    // console.log('not from handler');
-    // console.log(GoingToBeSplicedGoatObj);
-  }
+
   let singleGrab = GoingToBeSplicedGoatObj[Math.floor(Math.random() * GoingToBeSplicedGoatObj.length)];
   randosGrabbed.push(singleGrab);
 
@@ -66,6 +83,14 @@ var randoGoatImgGrabberFun = function(tester, fromHandler){
   let singleGrabIndexVal = GoingToBeSplicedGoatObj.indexOf(singleGrab);
   GoingToBeSplicedGoatObj.splice(singleGrabIndexVal, 1);
 
+  //ah! caught myself, i need to put these below the splice, because that's what i'm curious about.
+  if (fromHandler === false){
+    initialVals = GoingToBeSplicedGoatObj;
+  }
+
+  if (fromHandler === true){
+    handledVals = GoingToBeSplicedGoatObj;
+  }
 };
 
 var shuffleDeck = function(){
@@ -75,12 +100,66 @@ var shuffleDeck = function(){
 };
 shuffleDeck();
 
-var displayerFun = function (){
-  domImgGetters[0].src = randosGrabbed[0].src;
-  domImgGetters[1].src = randosGrabbed[1].src;
-  domImgGetters[2].src = randosGrabbed[2].src;
+
+
+var displayerFun = function (turnItOff){
+  if (!turnItOff){
+
+    //left
+    domImgGetters[0].src = randosGrabbed[0].src;
+    //middle
+    domImgGetters[1].src = randosGrabbed[1].src;
+    //right
+    domImgGetters[2].src = randosGrabbed[2].src;
+  }
+  else if(turnItOff === true){
+    console.log(turnItOff, 'true');
+    //left
+    // domImgGetters[0].src = '';
+    domImgGetters[0].style.borderStyle = 'none';
+
+    //middle
+    // domImgGetters[1].src = '';
+    domImgGetters[0].style.borderStyle = 'none';
+
+    //right
+    // domImgGetters[2].src = '';
+    domImgGetters[0].style.borderStyle = 'none';
+
+  }
 };
-displayerFun();
+displayerFun(false);
+
+var herehere = 'herehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehereherehere';
+
+var everyOther = function(){
+  var oldDisplay = randosGrabbed.map(ele => ele);
+  randosGrabbed = [];
+  shuffleDeck();
+  var newDisplay = randosGrabbed;
+  let duplicates = false;
+  let worked;
+
+
+  var lookForDuplicates = function(){
+    newDisplay.forEach(item => {
+      if (oldDisplay.includes(item)){
+        duplicates = true;
+      }
+      else duplicates = false;
+    });
+  };
+  while (duplicates === true){
+    randosGrabbed = [];
+    shuffleDeck();
+    newDisplay = randosGrabbed;
+    lookForDuplicates();
+  }
+  if (duplicates === false){
+    worked = true;
+  }
+};
+
 
 for (let get in domImgGetters){
   domImgGetters[get].height = '200';
@@ -155,12 +234,113 @@ var forTestingPurposes = function(){
 //ok, the following is hard coded version, just to get up and running i suppose, but really it shouldn't increment an arbitrary image counter, it should increment this.likes.
 
 
+var grabForDisplay = document.getElementById('lis');
+var wrappingItUp = function(){
+  grabForDisplay.textContent = 'For Display';
+  for(var i = 0; i < goatArr.length; i++){
+    var displayForEach = document.createElement('li');
+    displayForEach.textContent = `${goatArr[i].name} received  ${goatArr[i].likes} likes`;
+    grabForDisplay.appendChild(displayForEach);
+    displayerFun(true);
+    dataLikes.push(goatArr[i].likes);
+    dataSeen.push(goatArr[i].timesShown);
+    console.log(dataLikes, 'likes');
+    console.log(dataSeen, 'seen');
+  }
+}
 
 
-var totalCLicks = 0;
+
+//     var ctx = document.getElementById('myCanvas').getContext('2d');
+//     var myChart = new myChart();
+//     var myChart = new Chart(ctx, {
+//       type: 'bar',
+//         labels: labelsOfNames,
+//         datasets: [{
+//           label: '# of Votes',
+//           dataLikes: [12, 19, 3, 5, 2, 3],
+//           backgroundColor: [
+//             'rgba(255, 99, 132, 0.2)',
+//             'rgba(54, 162, 235, 0.2)',
+//             'rgba(255, 206, 86, 0.2)',
+//             'rgba(75, 192, 192, 0.2)',
+//             'rgba(153, 102, 255, 0.2)',
+//             'rgba(255, 159, 64, 0.2)'
+//           ],
+//           borderColor: [
+//             'rgba(255,99,132,1)',
+//             'rgba(54, 162, 235, 1)',
+//             'rgba(255, 206, 86, 1)',
+//             'rgba(75, 192, 192, 1)',
+//             'rgba(153, 102, 255, 1)',
+//             'rgba(255, 159, 64, 1)'
+//           ],
+//           borderWidth: 1
+//     )}]
+//       },
+//       options: {
+//         scales: {
+//           yAxes: [{
+//             ticks: {
+//               beginAtZero:arg
+//             }
+//           }]
+//         }
+      
+//     });
+
+//   }
+// };
+
+// var myChart = new Chart(ctx, {
+//   type: 'bar',
+//   data: {
+//       labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//       datasets: [{
+//           label: '# of Votes',
+//           data: [12, 19, 3, 5, 2, 3],
+//           backgroundColor: [
+//               'rgba(255, 99, 132, 0.2)',
+//               'rgba(54, 162, 235, 0.2)',
+//               'rgba(255, 206, 86, 0.2)',
+//               'rgba(75, 192, 192, 0.2)',
+//               'rgba(153, 102, 255, 0.2)',
+//               'rgba(255, 159, 64, 0.2)'
+//           ],
+//           borderColor: [
+//               'rgba(255,99,132,1)',
+//               'rgba(54, 162, 235, 1)',
+//               'rgba(255, 206, 86, 1)',
+//               'rgba(75, 192, 192, 1)',
+//               'rgba(153, 102, 255, 1)',
+//               'rgba(255, 159, 64, 1)'
+//           ],
+//           borderWidth: 1
+//       }]
+//   },
+//   options: {
+//       scales: {
+//           yAxes: [{
+//               ticks: {
+//                   beginAtZero:true
+//               }
+//           }]
+//       }
+//   }
+// });
+
+
+
+let counter = 0;
 var clickHandler = function(event){
-
+  totalCounts++;
   var whatWasClicked = (event.target);
+
+  //set source image tags
+
+  randosGrabbed[0].timesShown++;
+  randosGrabbed[1].timesShown++;
+  randosGrabbed[2].timesShown++;
 
   if(whatWasClicked.id === 'firstPic'){
     randosGrabbed[0].likes++;
@@ -173,35 +353,19 @@ var clickHandler = function(event){
     randosGrabbed[2].likes++;
   }
 
-  totalCLicks++;
-  //resets Randos to an empty slate.
+  counter++;
+  historyOfRandos.push({'randos' : randosGrabbed, 'counter' : counter});
 
-  //want to ACP and put randosGrabbed, and GoingToBeSplicedGoatObj = goatArr.map(ele => ele);
-  // inside randoGoatImgGrabberFun() with some if logic, if randosGrabbed is full, then do a refresh, else if empty proceed as usual.
-  //on second thought, lets keep it how it is for now...
-  historyOfRandos.push(randosGrabbed);
-  randosGrabbed = [];
+  everyOther();
+  displayerFun(false);
 
-
-  //reassigns to initial value
-  //commenting out shuffledeck for now
-  //shuffleDeck();
-  //herehere
-
-
-
-
-  
-  randoGoatImgGrabberFun(null,true);
-  randoGoatImgGrabberFun(null,true);
-  randoGoatImgGrabberFun(null,true);
-  displayerFun();
-  GoingToBeSplicedGoatObj = goatArr.map(ele => ele);
+  if (totalCounts >= 7){
+    getImgDiv.removeEventListener('click', clickHandler);
+    wrappingItUp();
+  }
 };
 
 getImgDiv.addEventListener('click', clickHandler);
-
-
 
 // shut listener off
 //make chart appear
@@ -209,54 +373,15 @@ getImgDiv.addEventListener('click', clickHandler);
 
 // charts
 
-var renderChart = function(){
-//chart needs ctx
+// var renderChart = function(){
+// //chart needs ctx
 
-  //collect all data --- we need labels, dataValues, colors,
+//   //collect all dataLikes --- we need labelsOfNames:  labelsdataValues, colors,
 
-  //create a data object that gets passed all our other arrays, based off the example from chartjs
-
-
-
-  //call a new chart and pass in ctx and our data.
-
-};
+//   //create a dataLikes object that gets passed all our other arrays, based off the example from chartjs
 
 
-var ctx = document.getElementById('myCanvas').getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero:arg
-        }
-      }]
-    }
-  }
-});
+
+//   //call a new chart and pass in ctx and our dataLikes.
+
+// };
---
+++ user_stories.md
@@ -0,0 +1,5 @@
+As a coder i want to experiment with neat functions. 
+
+As a marketer i want to be able to track which products the focus group people like about our products so we can make good business decisions about which products to include on the final product of our catalogue...
+
+As focus grouper i want to be able see good products and weed out bad ones. 
